ef49ee53b3c1508d049ab4e1033859b85f5e5e99
Victor Manuel Huecas Sánchez de Molina <vmhuecas@cestel.es>
2024-03-30 18:50:05 +0100

    Primer commit

---
+++ src/test/java/com/hollow/HollowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hollow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HollowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
