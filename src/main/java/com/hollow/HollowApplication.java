f92845462b82b2149ddd29f4b2b0e5c0734d44f5
Victor Manuel Huecas Sánchez de Molina <vmhuecas@cestel.es>
2024-03-30 19:41:23 +0100

    demo

--- src/main/java/com/hollow/HollowApplication.java
+++ src/main/java/com/hollow/HollowApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HollowApplication {
 
 	public static void main(String[] args) {
-		//demo
+		
 		SpringApplication.run(HollowApplication.class, args);
 	}
 
